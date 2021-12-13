187689bdf504d2c2c0fc056251d3e189487c3264
Algirdas Stricka <algirdasstricka@yahoo.com>
2021-12-13 15:22:31 +0200

    login success

--- src/API/v1/auth.js
+++ src/API/v1/auth.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { dbGetAction, dbFail, dbSuccess } = require('../../utils/dbHelper');
 const Joi = require('joi');
 const { token } = require('morgan');
-const {hashValue} = require('../../utils/hashHelper');
+const {hashValue, verifyHash} = require('../../utils/hashHelper');
+const jwt = require('jsonwebtoken');
+const {jwtSecret} = require('../../config');
 
 router.post('/register', async (req, res) => {
     const newUser = {
@@ -24,8 +26,24 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const
-    res.send({ msg: 'login success', token: '' });
+    const sql = 'SELECT * FROM users WHERE email =?';
+    const dbResult = await dbGetAction(sql, [req.body.email]);
+
+    if (dbResult.length !== 1) {
+        return dbFail(res, 'bad credentials');
+    }
+
+    if (!verifyHash(req.body.password, dbResult[0].password)) {
+        return dbFail(res, 'bad credentials');
+    }
+
+    const token = jwt.sign({ email: req.body.email }, jwtSecret, {expiresIn: '1h'});
+    console.log('token =', token);
+    const loggedInUser = {
+        email: req.body.email,
+        token: token
+    }
+    res.send({ msg: 'login success', loggedInUser });
 });
 
 module.exports = router;
